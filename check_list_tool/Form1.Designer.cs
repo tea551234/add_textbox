5771526d61757356230e9b204ee500ffa55de788
tea551234 <tea551234@gmail.com>
2023-04-25 00:07:03 +0800

    initial commit

---
+++ check_list_tool/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace check_list_tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.讀取CSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.讀取CSVToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.清除CSVToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.讀取CSVToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(806, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 讀取CSVToolStripMenuItem
+            // 
+            this.讀取CSVToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.讀取CSVToolStripMenuItem1,
+            this.清除CSVToolStripMenuItem1});
+            this.讀取CSVToolStripMenuItem.Name = "讀取CSVToolStripMenuItem";
+            this.讀取CSVToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.讀取CSVToolStripMenuItem.Text = "CSV相關";
+            // 
+            // 讀取CSVToolStripMenuItem1
+            // 
+            this.讀取CSVToolStripMenuItem1.Name = "讀取CSVToolStripMenuItem1";
+            this.讀取CSVToolStripMenuItem1.Size = new System.Drawing.Size(121, 22);
+            this.讀取CSVToolStripMenuItem1.Text = "讀取CSV";
+            this.讀取CSVToolStripMenuItem1.Click += new System.EventHandler(this.讀取CSVToolStripMenuItem1_Click);
+            // 
+            // 清除CSVToolStripMenuItem1
+            // 
+            this.清除CSVToolStripMenuItem1.Name = "清除CSVToolStripMenuItem1";
+            this.清除CSVToolStripMenuItem1.Size = new System.Drawing.Size(121, 22);
+            this.清除CSVToolStripMenuItem1.Text = "清除CSV";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(806, 312);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 讀取CSVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 讀取CSVToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 清除CSVToolStripMenuItem1;
+    }
+}
+
---
+++ check_list_tool/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace check_list_tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadCSVFile(string filePath)
+        {
+            int numControls = 1; // 记录已添加的控件数目
+            int formWidth = 800; // 默认Form宽度
+            int formHeight = 600; // 默认Form高度
+
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    // 检查是否存在对应的面板
+                    if (!this.Controls.ContainsKey(values[0] + "Panel"))
+                    {
+                        // 创建一个新的面板
+                        var panel = new Panel();
+                        panel.Name = values[0] + "Panel";
+                        panel.Location = new Point(10, 10 + (35 * numControls));
+                        panel.Size = new Size(700, 30);
+                        this.Controls.Add(panel);
+
+                        // 创建一个新的标签
+                        var label = new Label();
+                        label.Text = values[0];
+                        label.Location = new Point(0, 7);
+                        label.AutoSize = true;
+                        panel.Controls.Add(label);
+
+                        _addtextbox(values, panel);
+                        numControls++; // 添加控件计数器加1
+
+                        // 检查是否需要调整Form的大小
+                        if (numControls * 35 + 10 > formHeight)
+                        {
+                            formHeight = numControls * 35 + 10;
+                        }
+                    }
+                }
+            }
+
+            // 调整Form的大小
+            this.Size = new Size(formWidth, formHeight);
+        }
+
+        private void _addtextbox(string[] values, Panel panel)
+        {
+            var textBox1 = CreateTextBox(values[0] + "TextBox1", 50, 5);
+            var textBox2 = CreateTextBox(values[0] + "TextBox2", 150, 5);
+            var textBox3 = CreateTextBox(values[0] + "TextBox3", 250, 5);
+            panel.Controls.AddRange(new Control[] { textBox1, textBox2, textBox3 });
+        }
+
+        private TextBox CreateTextBox(string name, int x, int y)
+        {
+            var textBox = new TextBox();
+            textBox.Name = name;
+            textBox.Location = new Point(x, y);
+            textBox.Size = new Size(80, 20);
+            return textBox;
+        }
+
+
+        private void 讀取CSVToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                LoadCSVFile(filePath);
+            }
+        }
+    }
+}
